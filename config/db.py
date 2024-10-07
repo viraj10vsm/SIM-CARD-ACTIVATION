018472bbae4d428b32ce6806109d667ecac2ea4b
viraj10vsm <mahaleviraj1018@gmail.com>
2024-10-07 13:57:49 +0530

    Cloud DB

--- config/db.py
+++ config/db.py
@@ -1,2 +1,5 @@
 from pymongo import MongoClient
-conn = MongoClient()
+from pymongo.server_api import ServerApi
+db_password = "oRFJJy3la3ocx1EU"
+uri = f'mongodb+srv://mahaleviraj1018:{db_password}@simact.5hd2h.mongodb.net/?retryWrites=true&w=majority&appName=SIMACT'
+conn = MongoClient(uri, server_api=ServerApi('1'))
